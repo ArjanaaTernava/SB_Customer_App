367827f896308745ac58b141a81b9e34b4fff0b6
ArjanaaTernava <arjana.ternava@evonem.com>
2023-06-21 10:49:43 +0200

    Added customer service injection in controller

--- src/main/java/com/intro/springbootapplication/customer/CustomerController.java
+++ src/main/java/com/intro/springbootapplication/customer/CustomerController.java
@@ -5,8 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
     @GetMapping
     Customer getCustomer(){
-        return new Customer(1L,"John");
+        return customerService.getCustomer();
     }
 }
--- src/main/java/com/intro/springbootapplication/customer/CustomerService.java
+++ src/main/java/com/intro/springbootapplication/customer/CustomerService.java
@@ -1,4 +1,12 @@
 package com.intro.springbootapplication.customer;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CustomerService {
+
+
+    public Customer getCustomer(){
+        return new Customer(1L,"John");
+    }
 }
