eb32ac58381ef40fbf8cf47fbdbd6486208f48cc
ArjanaaTernava <arjana.ternava@evonem.com>
2023-06-21 14:37:40 +0200

    added a new version for customer controller and added annotation for the old one

--- src/main/java/com/intro/springbootapplication/customer/CustomerController2.java
+++ src/main/java/com/intro/springbootapplication/customer/CustomerController2.java
@@ -1,25 +1,26 @@
 package com.intro.springbootapplication.customer;
 
+
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(path ="api/v1/customer")
-@Deprecated
-public class CustomerController {
+@RequestMapping(path ="api/v2/customer")
+public class CustomerController2 {
     private final CustomerService customerService;
 
-    public CustomerController(CustomerService customerService) {
+    public CustomerController2(CustomerService customerService) {
         this.customerService = customerService;
     }
 
     @GetMapping
     List<Customer> getCustomers(){
-        return List.of(new Customer(1L,"IMPLEMENT DB","NO_PASSWORD"));
+        return List.of(new Customer(1L,"JAMES BOND","NO_PASSWORD"));
     }
 
-    @PostMapping
+    @PostMapping("all")
     void registerNewCustomer(@RequestBody Customer customer){
         System.out.println("POST REQUEST...");
         System.out.println(customer);
