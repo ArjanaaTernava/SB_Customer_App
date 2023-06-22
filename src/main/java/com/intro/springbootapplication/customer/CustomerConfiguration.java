b6f943670ef1ee26598e3ee878317d09fe88595f
ArjanaaTernava <arjana.ternava@evonem.com>
2023-06-22 11:00:59 +0200

    Added info app

--- src/main/java/com/intro/springbootapplication/customer/CustomerConfiguration.java
+++ src/main/java/com/intro/springbootapplication/customer/CustomerConfiguration.java
@@ -1,9 +1,12 @@
 package com.intro.springbootapplication.customer;
 
+import com.intro.springbootapplication.infoapp.InfoApp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class CustomerConfiguration {
@@ -11,10 +14,14 @@ public class CustomerConfiguration {
     @Value("${useFakeCustomerRepo}")
     private boolean useFakeCustomerRepo;
 
+    @Autowired
+    private Environment environment;
     @Bean
-    CommandLineRunner commandLineRunner(){
+    CommandLineRunner commandLineRunner(InfoApp infoApp){
         return args -> {
             System.out.println("hello from customer configuration");
+            System.out.println(environment.getProperty("useFakeCustomerRepo"));
+            System.out.println(infoApp);
         };
     }
 
