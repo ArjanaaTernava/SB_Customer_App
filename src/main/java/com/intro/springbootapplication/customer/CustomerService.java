6f098d0178952e9d001d852c555b47c405fd3d04
ArjanaaTernava <arjana.ternava@evonem.com>
2023-06-22 10:35:40 +0200

    Added logger for customer service and api exception handler

--- src/main/java/com/intro/springbootapplication/customer/CustomerService.java
+++ src/main/java/com/intro/springbootapplication/customer/CustomerService.java
@@ -1,5 +1,8 @@
 package com.intro.springbootapplication.customer;
 import com.intro.springbootapplication.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -7,17 +10,24 @@ import java.util.Objects;
 
 @Service
 public class CustomerService {
-    private final CustomerSecondRepository customerRepository;
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+    private final CustomerSecondRepository customerRepository;
+    @Autowired
     public CustomerService(CustomerSecondRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
 
     List<Customer> getCustomers(){
+        LOGGER.info("Method getCustomers was called");
         return customerRepository.findAll();
     }
 
     Customer getCustomerById(Long customerId){
-        return getCustomers().stream().filter(customer -> Objects.equals(customer.getId(), customerId)).findFirst().orElseThrow(()-> new NotFoundException("Customer" + customerId + "does not exist"));
+        return customerRepository.findById(customerId).orElseThrow(()-> {
+            NotFoundException notFoundException = new NotFoundException("Customer" + customerId + "does not exist");
+            LOGGER.error("Customer with id {} not found, class: {}", customerId, CustomerService.class, notFoundException);
+            return notFoundException;
+        });
     }
 }
